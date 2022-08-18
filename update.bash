faa40f55791a2ea4dea534ca12e86772484bf398
kumar-devesh <kumar_d@cs.iitr.ac.in>
2022-08-18 22:38:59 +0530

    update resume

--- update.bash
+++ update.bash
@@ -1,21 +1,15 @@
 #!/bin/bash
-
-if [ -z "$1" ]; then
+if [ -z "$1" ] 
+then
   echo "You need to specify the changes to commit as an argument"
   exit 1
 else
   git add -A
-  #git commit -am "$1"
   git commit --amend -m "$1"
   git push --force
-
   hugo
-
   cd public
   git add -A
-  #git commit -am "$1"
   git commit --amend -m "$1"
   git push --force
-
-  cd ..
 fi
